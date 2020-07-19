34eb0c0ad6f6b71c774829a252250d7aa9b6942e
sherwin <sherwin.water@gmail.com>
2020-07-19 18:22:20 -0400

    Created entities and refactored the project using Lombok

---
+++ src/main/java/com/sherwin/springit/domain/Comment.java
@@ -0,0 +1,22 @@
+package com.sherwin.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+    // link
+
+}
